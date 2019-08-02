8a12e941483f0a02939798519cd7d01b71d433f2
khai96_ <hvksmr1996@gmail.com>
2019-08-02 21:35:40 +0700

    Create @khai96x/interval-event-pool-universe

---
+++ packages/interval-event-pool-universe/index.ts
@@ -0,0 +1,30 @@
+import { createEventPool, some } from '@khai96x/simple-event-pool'
+import AdvMapInit from 'advanced-map-initialized'
+
+const INTERVAL_EVENT = Symbol('INTERVAL_EVENT')
+
+const universe = new AdvMapInit(
+  Map,
+  (delay: number) => createEventPool({
+    setInterval,
+    clearInterval,
+    delay
+  })
+    .createAutoTrigger(INTERVAL_EVENT, param => {
+      return some(new IntervalEventParam(
+        delay,
+        param.iterationCount
+      ))
+    })
+)
+
+class IntervalEventParam {
+  constructor (
+    public readonly internalDelay: number,
+    public readonly internalCount: number
+  ) {}
+}
+
+export function get (delay: number) {
+  return universe.get(delay)
+}
