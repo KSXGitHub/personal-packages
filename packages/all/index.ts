c4b43ae4e0ab74ef6bdf9319c8f52c2af5486ad5
khai96_ <hvksmr1996@gmail.com>
2019-08-03 12:24:49 +0700

    Create @khai96x/all

---
+++ packages/all/index.ts
@@ -0,0 +1,8 @@
+import * as simpleEventPool from '@khai96x/simple-event-pool'
+export { simpleEventPool }
+
+import * as intervalEventPoolUniverse from '@khai96x/interval-event-pool-universe'
+export { intervalEventPoolUniverse }
+
+import * as myEvents from '@khai96x/my-events'
+export { myEvents }
