076411e702a8cceb9aa96a251b2dc67a0e404d75
khai96_ <hvksmr1996@gmail.com>
2019-08-04 20:40:49 +0700

    Share another way

--- packages/interval-observable-universe/index.ts
+++ packages/interval-observable-universe/index.ts
@@ -1,7 +1,6 @@
 import { Observable, OperatorFunction, interval } from 'rxjs'
 import * as operators from 'rxjs/operators'
 import AdvMapInit from 'advanced-map-initialized'
-const { share } = operators
 
 const universe = new AdvMapInit(Map, (period: number) => interval(period))
 
@@ -14,7 +13,7 @@ export default getIntervalObservable
 export function pipeline<X, Y> (fn: (x: X) => Observable<Y>) {
   return {
     to<Z> (opt: OperatorFunction<Y, Z>) {
-      return pipeline((x: X) => fn(x).pipe(share()).pipe(opt))
+      return pipeline((x: X) => fn(x).pipe(opt))
     },
 
     fn
