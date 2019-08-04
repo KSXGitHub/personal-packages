e8ce25e876b3eef4f8678e9f52647425710b99f8
khai96_ <hvksmr1996@gmail.com>
2019-08-04 21:11:12 +0700

    Change where it share()

--- packages/interval-observable-universe/index.ts
+++ packages/interval-observable-universe/index.ts
@@ -7,9 +7,9 @@ export const getIntervalObservable = pipeline((period: number) => interval(perio
 export default getIntervalObservable
 
 export function pipeline<X, Y> (fn: (x: X) => Observable<Y>) {
-  const map = new AdvMapInit(Map, (x: X) => fn(x).pipe(share()))
+  const map = new AdvMapInit(Map, (x: X) => fn(x))
   const to = <Z> (opt: OperatorFunction<Y, Z>) =>
-    pipeline((x: X) => map.get(x).pipe(opt))
+    pipeline((x: X) => map.get(x).pipe(opt).pipe(share()))
   return { fn, to }
 }
 
