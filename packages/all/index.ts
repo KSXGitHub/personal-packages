9e293b69d3ce49d55c16592b450f67132642f755
khai96_ <hvksmr1996@gmail.com>
2019-10-05 15:39:28 +0700

    Add bumpPackageVersions to all/index.ts

--- packages/all/index.ts
+++ packages/all/index.ts
@@ -3,3 +3,6 @@ export { intervalObservableUniverse }
 
 import * as myObservables from '@khai96x/my-observables'
 export { myObservables }
+
+import * as bumpPackageVersions from '@khai96x/bump-package-versions'
+export { bumpPackageVersions }
