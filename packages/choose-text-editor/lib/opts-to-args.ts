2bbf5dd7a09e07e0ff1fa737ec93c95c7c3be36c
khai96_ <hvksmr1996@gmail.com>
2019-12-31 11:40:30 +0700

    Update opts2args to work on boolean

--- packages/choose-text-editor/lib/opts-to-args.ts
+++ packages/choose-text-editor/lib/opts-to-args.ts
@@ -3,6 +3,9 @@ import flag2arg from './flag-to-arg'
 
 export const opts2args = (options: EditorOptions) => Object
   .entries(options)
-  .map(([key, value]) => flag2arg(key) + '=' + value)
+  .map(([key, value]) => encodeSingleOption(key, value))
+
+export const encodeSingleOption = (key: string, value: string | number | boolean) =>
+  flag2arg(key) + (typeof value === 'boolean' ? '' : '=' + value)
 
 export default opts2args
