61c0329dab9c01bd773ea2117793e14ae302a023
khai96_ <hvksmr1996@gmail.com>
2020-01-01 15:18:26 +0700

    Create some tests

---
+++ test/choose-text-editor/encode-cli-flag.test.ts
@@ -0,0 +1,32 @@
+import { encodeCliFlag } from '@khai96x/choose-text-editor'
+
+it('matches snapshot', () => {
+  const result = [
+    'foo',
+    'foo-bar',
+    'fooBar',
+    'f',
+    'a'
+  ].map(input => ({
+    input,
+    output: encodeCliFlag(input)
+  }))
+
+  expect(result).toMatchSnapshot()
+})
+
+it('works as intended', () => {
+  expect([
+    'foo',
+    'foo-bar',
+    'fooBar',
+    'f',
+    'a'
+  ].map(encodeCliFlag)).toEqual([
+    '--foo',
+    '--foo-bar',
+    '--fooBar',
+    '-f',
+    '-a'
+  ])
+})
---
+++ test/choose-text-editor/encode-cli-options.test.ts
@@ -0,0 +1,48 @@
+import { encodeCliOptions } from '@khai96x/choose-text-editor'
+
+it('matches snapshot', () => {
+  expect(encodeCliOptions({
+    stringOption: 'abc',
+    numberOption: 123,
+    trueOption: true,
+    falseOption: false,
+    arrayOption: ['abc', 'def', 123, 456],
+    a: 'foo',
+    b: 456,
+    c: true,
+    d: false,
+    e: ['a', 0, 'b', 1, 2]
+  })).toMatchSnapshot()
+})
+
+it('works as intended', () => {
+  expect(encodeCliOptions({
+    stringOption: 'abc',
+    numberOption: 123,
+    trueOption: true,
+    falseOption: false,
+    arrayOption: ['abc', 'def', 123, 456],
+    a: 'foo',
+    b: 456,
+    c: true,
+    d: false,
+    e: ['a', 0, 'b', 1, 2]
+  })).toEqual([
+    '--stringOption', 'abc',
+    '--numberOption', '123',
+    '--trueOption',
+    '--arrayOption', 'abc', 'def', '123', '456',
+    '-a', 'foo',
+    '-b', '456',
+    '-c',
+    '-e', 'a', '0', 'b', '1', '2'
+  ])
+})
+
+it('false boolean does not appear', () => {
+  expect(encodeCliOptions({ flag: false })).not.toContain('--flag')
+})
+
+it('true boolean appears without value', () => {
+  expect(encodeCliOptions({ flag: true })).toEqual(['--flag'])
+})
