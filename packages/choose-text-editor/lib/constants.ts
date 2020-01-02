e573a20a9dd4238bba2c8cc0a96dda4c2604bb6e
khai96_ <hvksmr1996@gmail.com>
2020-01-02 13:09:39 +0700

    Add PACKAGE_VERSION

--- packages/choose-text-editor/lib/constants.ts
+++ packages/choose-text-editor/lib/constants.ts
@@ -1,6 +1,7 @@
 export const PACKAGE_JSON = require('../package.json')
 
 export const PACKAGE_NAME = PACKAGE_JSON.name
+export const PACKAGE_VERSION = PACKAGE_JSON.version
 
 export const SEARCH_PLACES = [
   'choose-text-editor',
