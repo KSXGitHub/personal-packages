74c880d834121bb5d7d3d375abd2193ada3e05f8
khai96_ <hvksmr1996@gmail.com>
2020-01-02 15:22:41 +0700

    Complete test for validateChooser

--- test/choose-text-editor/validate.test.ts
+++ test/choose-text-editor/validate.test.ts
@@ -42,7 +42,7 @@ describe('validateChooser', () => {
       })
     })
 
-    describe(`package name and exact version`, () => {
+    describe('package name and exact version', () => {
       const chooser = '@khai96x/choose-text-editor@3.2.1'
 
       it('returns true', () => {
@@ -66,7 +66,7 @@ describe('validateChooser', () => {
       })
     })
 
-    describe(`package name and tilde version`, () => {
+    describe('package name and tilde version', () => {
       const chooser = '@khai96x/choose-text-editor@~3.2.0'
 
       it('returns true', () => {
@@ -90,7 +90,7 @@ describe('validateChooser', () => {
       })
     })
 
-    describe(`package name and caret version`, () => {
+    describe('package name and caret version', () => {
       const chooser = '@khai96x/choose-text-editor@^3.0.0'
 
       it('returns true', () => {
@@ -111,6 +111,165 @@ describe('validateChooser', () => {
 
       it('does not call callbacks.onUnsatisfiedVersion', () => {
         expect(setup(chooser).callbacks.onUnsatisfiedVersion).not.toBeCalled()
+      })
+    })
+  })
+
+  describe('when chooser package name is different', () => {
+    describe('package name and caret version', () => {
+      const chooser = 'different@^3.0.0'
+
+      it('returns false', () => {
+        expect(setup(chooser).result).toBe(false)
+      })
+
+      it('calls callbacks.onInvalidPackageName exactly once', () => {
+        expect(setup(chooser).callbacks.onInvalidPackageName).toBeCalledTimes(1)
+      })
+
+      it('calls callbacks.onInvalidPackageName with package names', () => {
+        expect(setup(chooser).callbacks.onInvalidPackageName).toBeCalledWith(
+          expect.any(String),
+          PACKAGE_NAME
+        )
+      })
+
+      it('does not call callbacks.onNonEmptyPath', () => {
+        expect(setup(chooser).callbacks.onNonEmptyPath).not.toBeCalled()
+      })
+
+      it('does not call callbacks.onInvalidVersionRange', () => {
+        expect(setup(chooser).callbacks.onInvalidVersionRange).not.toBeCalled()
+      })
+
+      it('does not call callbacks.onUnsatisfiedVersion', () => {
+        expect(setup(chooser).callbacks.onUnsatisfiedVersion).not.toBeCalled()
+      })
+    })
+  })
+
+  describe('when chooser package name contains path', () => {
+    describe('package name and caret version', () => {
+      const path = 'path/after/package/name'
+      const chooser = PACKAGE_NAME + '/' + path
+
+      it('returns false', () => {
+        expect(setup(chooser).result).toBe(false)
+      })
+
+      it('does not call callbacks.onInvalidPackageName', () => {
+        expect(setup(chooser).callbacks.onInvalidPackageName).not.toBeCalled()
+      })
+
+      it('calls callbacks.onNonEmptyPath exactly once', () => {
+        expect(setup(chooser).callbacks.onNonEmptyPath).toBeCalledTimes(1)
+      })
+
+      it('calls callbacks.onNonEmptyPath with path', () => {
+        expect(setup(chooser).callbacks.onNonEmptyPath).toBeCalledWith(path)
+      })
+
+      it('does not call callbacks.onInvalidVersionRange', () => {
+        expect(setup(chooser).callbacks.onInvalidVersionRange).not.toBeCalled()
+      })
+
+      it('does not call callbacks.onUnsatisfiedVersion', () => {
+        expect(setup(chooser).callbacks.onUnsatisfiedVersion).not.toBeCalled()
+      })
+    })
+  })
+
+  describe('when chooser version range is invalid', () => {
+    const versionRange = 'InvalidVersionRange'
+    const chooser = PACKAGE_NAME + '@' + versionRange
+
+    it('returns false', () => {
+      expect(setup(chooser).result).toBe(false)
+    })
+
+    it('does not call callbacks.onInvalidPackageName', () => {
+      expect(setup(chooser).callbacks.onInvalidPackageName).not.toBeCalled()
+    })
+
+    it('does not call callbacks.onNonEmptyPath', () => {
+      expect(setup(chooser).callbacks.onNonEmptyPath).not.toBeCalled()
+    })
+
+    it('calls callbacks.onInvalidVersionRange exactly once', () => {
+      expect(setup(chooser).callbacks.onInvalidVersionRange).toBeCalledTimes(1)
+    })
+
+    it('calls callbacks.onInvalidVersionRange with invalid version range', () => {
+      expect(setup(chooser).callbacks.onInvalidVersionRange).toBeCalledWith(versionRange)
+    })
+
+    it('does not call callbacks.onUnsatisfiedVersion', () => {
+      expect(setup(chooser).callbacks.onUnsatisfiedVersion).not.toBeCalled()
+    })
+  })
+
+  describe('when chooser version is not satisfied', () => {
+    describe('chooser version range > used version', () => {
+      const versionRange = '>3.2.1'
+      const chooser = `@khai96x/choose-text-editor@${versionRange}`
+
+      it('returns false', () => {
+        expect(setup(chooser).result).toBe(false)
+      })
+
+      it('does not call callbacks.onInvalidPackageName', () => {
+        expect(setup(chooser).callbacks.onInvalidPackageName).not.toBeCalled()
+      })
+
+      it('does not call callbacks.onNonEmptyPath', () => {
+        expect(setup(chooser).callbacks.onNonEmptyPath).not.toBeCalled()
+      })
+
+      it('does not call callbacks.onInvalidVersionRange', () => {
+        expect(setup(chooser).callbacks.onInvalidVersionRange).not.toBeCalled()
+      })
+
+      it('calls callbacks.onUnsatisfiedVersion exactly once', () => {
+        expect(setup(chooser).callbacks.onUnsatisfiedVersion).toBeCalledTimes(1)
+      })
+
+      it('calls callbacks.onUnsatisfiedVersion with chooser version range and used version', () => {
+        expect(setup(chooser).callbacks.onUnsatisfiedVersion).toBeCalledWith(
+          versionRange,
+          PACKAGE_VERSION
+        )
+      })
+    })
+
+    describe('chooser version range < used version', () => {
+      const versionRange = '<3.2.1'
+      const chooser = `@khai96x/choose-text-editor@${versionRange}`
+
+      it('returns false', () => {
+        expect(setup(chooser).result).toBe(false)
+      })
+
+      it('does not call callbacks.onInvalidPackageName', () => {
+        expect(setup(chooser).callbacks.onInvalidPackageName).not.toBeCalled()
+      })
+
+      it('does not call callbacks.onNonEmptyPath', () => {
+        expect(setup(chooser).callbacks.onNonEmptyPath).not.toBeCalled()
+      })
+
+      it('does not call callbacks.onInvalidVersionRange', () => {
+        expect(setup(chooser).callbacks.onInvalidVersionRange).not.toBeCalled()
+      })
+
+      it('calls callbacks.onUnsatisfiedVersion exactly once', () => {
+        expect(setup(chooser).callbacks.onUnsatisfiedVersion).toBeCalledTimes(1)
+      })
+
+      it('calls callbacks.onUnsatisfiedVersion with chooser version range and used version', () => {
+        expect(setup(chooser).callbacks.onUnsatisfiedVersion).toBeCalledWith(
+          versionRange,
+          PACKAGE_VERSION
+        )
       })
     })
   })
