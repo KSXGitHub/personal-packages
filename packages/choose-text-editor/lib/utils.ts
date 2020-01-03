0fb6a74c18b60e76f4b07909b298749737309d0c
khai96_ <hvksmr1996@gmail.com>
2020-01-03 14:43:21 +0700

    Create concatWithLength

---
+++ packages/choose-text-editor/lib/utils.ts
@@ -0,0 +1,23 @@
+import { concat } from 'iter-tools'
+
+interface IterableWithLength<Element> extends Iterable<Element> {
+  readonly length: number
+}
+
+class ConcatLengthResult<Element> implements IterableWithLength<Element> {
+  constructor (
+    private iterables: readonly IterableWithLength<Element>[]
+  ) {}
+
+  public get length () {
+    return this.iterables.reduce((acc, cur) => acc + cur.length, 0)
+  }
+
+  public [Symbol.iterator] () {
+    return concat(...this.iterables)
+  }
+}
+
+export function concatWithLength<Element> (...iterables: IterableWithLength<Element>[]) {
+  return new ConcatLengthResult(iterables)
+}
