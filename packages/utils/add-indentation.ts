890f3f9076d3ca6b081647c6f7c22b0e8eed6ec6
khai96_ <hvksmr1996@gmail.com>
2020-01-04 00:08:08 +0700

    Remove addIndentation as it is no longer used

--- packages/utils/add-indentation.ts
+++
@@ -1,6 +0,0 @@
-export function addIndentation (text: string, level: number) {
-  const indent = ' '.repeat(level)
-  return text.split('\n').map(line => indent + line).join('\n')
-}
-
-export default addIndentation
--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,3 +1,2 @@
-export * from './add-indentation'
 export * from './concat-with-length'
 export * from './to-string-array'
