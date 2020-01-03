8206451a95e4e61bf25f56b920eb47161b8aeef6
khai96_ <hvksmr1996@gmail.com>
2020-01-03 15:17:51 +0700

    Don't wrap an if(x) around a switch(x)

--- packages/choose-text-editor/lib/main.ts
+++ packages/choose-text-editor/lib/main.ts
@@ -130,37 +130,35 @@ export async function main<Return> (param: MainParam<Return>): Promise<Return> {
 
   const result = await param.choose({ env, which, editorSet })
 
-  if (result.error) {
-    switch (result.error) {
-      case (INDETERMINABLE_TTY):
-        logError('[ERROR] Cannot determine whether terminal is graphical or not')
-        logError('help: You may set ISINTTY=true to use terminal editors, or ISINTTY=false to use graphical editors')
-        return exit(Status.IndeterminableTTY)
-      case (NOT_FOUND):
-        logError('[ERROR] No editor detected')
-        logError('help: Check if (at least one of) your editors are installed')
-        logError('help: Check if there is any typo in your config')
-        return exit(Status.NotFound)
-      case (NO_EDITOR):
-        logError('[ERROR] No suitable editor')
-        logError('help: When ISINTTY=true, "terminal" property of your config must not be empty')
-        logError('help: When ISINTTY=false, either "graphical" or "terminal" property of your config must not be empty')
-        return exit(Status.EmptyEditorSet)
-      case (PREFIXES_PARSING_FAILURE):
-        logError('[ERROR] Failed to parse prefixes')
-        logError('help: Content must be a valid yaml array of strings')
-        logError(`* env key: ${result.envKey}`)
-        logError(`* env value: ${result.envValue}`)
-        logError(`* error: ${result.errorObject}`)
-        return exit(Status.InvalidPrefix)
-      case (INVALID_PREFIXES):
-        logError('[ERROR] Prefixes does not satisfied its schema')
-        logError('help: Instance must be an array of strings')
-        logError(`* env key: ${result.envKey}`)
-        logError(`* instance: ${result.instance}`)
-        // TODO: Print error messages from result.validatorResult.errors
-        return exit(Status.InvalidPrefix)
-    }
+  switch (result.error) {
+    case (INDETERMINABLE_TTY):
+      logError('[ERROR] Cannot determine whether terminal is graphical or not')
+      logError('help: You may set ISINTTY=true to use terminal editors, or ISINTTY=false to use graphical editors')
+      return exit(Status.IndeterminableTTY)
+    case (NOT_FOUND):
+      logError('[ERROR] No editor detected')
+      logError('help: Check if (at least one of) your editors are installed')
+      logError('help: Check if there is any typo in your config')
+      return exit(Status.NotFound)
+    case (NO_EDITOR):
+      logError('[ERROR] No suitable editor')
+      logError('help: When ISINTTY=true, "terminal" property of your config must not be empty')
+      logError('help: When ISINTTY=false, either "graphical" or "terminal" property of your config must not be empty')
+      return exit(Status.EmptyEditorSet)
+    case (PREFIXES_PARSING_FAILURE):
+      logError('[ERROR] Failed to parse prefixes')
+      logError('help: Content must be a valid yaml array of strings')
+      logError(`* env key: ${result.envKey}`)
+      logError(`* env value: ${result.envValue}`)
+      logError(`* error: ${result.errorObject}`)
+      return exit(Status.InvalidPrefix)
+    case (INVALID_PREFIXES):
+      logError('[ERROR] Prefixes does not satisfied its schema')
+      logError('help: Instance must be an array of strings')
+      logError(`* env key: ${result.envKey}`)
+      logError(`* instance: ${result.instance}`)
+      // TODO: Print error messages from result.validatorResult.errors
+      return exit(Status.InvalidPrefix)
   }
 
   /* HANDLE CHOSEN COMMAND */
