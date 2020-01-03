9a4a4a29f043e544600b3dc42017e5e66bd44909
khai96_ <hvksmr1996@gmail.com>
2020-01-03 21:14:34 +0700

    Create tests for @khai96x/utils

---
+++ test/utils/concat-with-length.test.ts
@@ -0,0 +1,102 @@
+import { concatWithLength } from '@khai96x/utils'
+
+describe('concatenation of arrays', () => {
+  describe('when no operand are mutated after concatenation', () => {
+    function init () {
+      const a = [0, 1, 2, 3]
+      const b = [4, 5]
+      const c = [7, 8, 9]
+      const abc = concatWithLength(a, b, c)
+      return { a, b, c, abc }
+    }
+
+    it('makes an iterable object', () => {
+      const { a, b, c, abc } = init()
+      expect(Array.from(abc)).toEqual([...a, ...b, ...c])
+    })
+
+    it('makes a "length" property', () => {
+      const { a, b, c, abc } = init()
+      expect(abc).toHaveProperty('length', a.length + b.length + c.length)
+    })
+  })
+
+  describe('when some operands are mutated after concatenation', () => {
+    function init () {
+      const a = [0, 1, 2, 3]
+      const b = [4, 5]
+      const c = [7, 8, 9]
+      const abc = concatWithLength(a, b, c)
+      a.push(123, 456)
+      b.pop()
+      return { a, b, c, abc }
+    }
+
+    it('updates its elements', () => {
+      const { a, b, c, abc } = init()
+      expect(Array.from(abc)).toEqual([...a, ...b, ...c])
+    })
+
+    it('updates its "length"', () => {
+      const { a, b, c, abc } = init()
+      expect(abc).toHaveProperty('length', a.length + b.length + c.length)
+    })
+  })
+})
+
+describe('concatenation of concatenations', () => {
+  describe('when no operand are mutated after concatenation', () => {
+    function init () {
+      const a = [0, 1, 2, 3]
+      const b = [4, 5]
+      const c = [7, 8, 9]
+      const d = ['abc', 'def', 'ghi']
+      const e = ['jkl']
+      const f = 'abc'
+      const abc = concatWithLength(a, b, c)
+      const de = concatWithLength(d, e)
+      const abcdef = concatWithLength<string | number>(abc, de, f)
+      return { a, b, c, d, e, f, abc, de, abcdef }
+    }
+
+    it('makes an iterable object', () => {
+      const { a, b, c, d, e, f, abcdef } = init()
+      expect(Array.from(abcdef)).toEqual([...a, ...b, ...c, ...d, ...e, ...f])
+    })
+
+    it('makes a "length" property', () => {
+      const { a, b, c, d, e, f, abcdef } = init()
+      const expectedLength = a.length + b.length + c.length + d.length + e.length + f.length
+      expect(Array.from(abcdef)).toHaveProperty('length', expectedLength)
+    })
+  })
+
+  describe('when some operands are mutated after concatenation', () => {
+    function init () {
+      const a = [0, 1, 2, 3]
+      const b = [4, 5]
+      const c = [7, 8, 9]
+      const d = ['abc', 'def', 'ghi']
+      const e = ['jkl']
+      const f = 'abc'
+      const abc = concatWithLength(a, b, c)
+      const de = concatWithLength(d, e)
+      const abcdef = concatWithLength<string | number>(abc, de, f)
+      a.push(123, 456, 789)
+      d.pop()
+      e.unshift('a', 'b', 'c')
+      return { a, b, c, d, e, f, abc, de, abcdef }
+    }
+
+    it('makes an iterable object', () => {
+      const { a, b, c, d, e, f, abcdef } = init()
+      expect(Array.from(abcdef)).toEqual([...a, ...b, ...c, ...d, ...e, ...f])
+    })
+
+    it('makes a "length" property', () => {
+      const { a, b, c, d, e, f, abcdef } = init()
+      const expectedLength = a.length + b.length + c.length + d.length + e.length + f.length
+      expect(Array.from(abcdef)).toHaveProperty('length', expectedLength)
+    })
+  })
+})
---
+++ test/utils/to-string-array.test.ts
@@ -0,0 +1,21 @@
+import { toStringArray } from '@khai96x/utils'
+
+it('works', () => {
+  expect(toStringArray([
+    'abc',
+    123,
+    {},
+    Infinity,
+    undefined,
+    null,
+    { toString: () => 'custom toString' }
+  ])).toEqual([
+    'abc',
+    '123',
+    '[object Object]',
+    'Infinity',
+    'undefined',
+    'null',
+    'custom toString'
+  ])
+})
