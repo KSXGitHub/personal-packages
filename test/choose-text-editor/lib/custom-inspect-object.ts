adebef744732f603914992cacec36d0f3c2c5488
khai96_ <hvksmr1996@gmail.com>
2020-01-09 16:19:55 +0700

    Create CustomInspectObject

---
+++ test/choose-text-editor/lib/custom-inspect-object.ts
@@ -0,0 +1,14 @@
+import { inspect } from 'util'
+
+export interface CustomInspectObject {
+  readonly [inspect.custom]: () => string
+}
+
+export const CustomInspectObject = (
+  msg: string | (() => string)
+): CustomInspectObject =>
+  typeof msg === 'string'
+    ? CustomInspectObject(() => msg)
+    : { [inspect.custom]: msg }
+
+export default CustomInspectObject
