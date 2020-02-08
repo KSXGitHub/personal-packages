6f4986d2231c60afdc7b734853dc595d52874ba8
khai96_ <hvksmr1996@gmail.com>
2020-02-08 10:45:20 +0700

    Delete unnecessary schema check

--- test/json-schemas/choose-text-editor/cli-arguments.test.ts
+++
@@ -1,8 +0,0 @@
-import { schemas } from '@khai96x/choose-text-editor'
-import { schemaTester } from '../lib/test-schema'
-
-it('EditorSet matches its JSON Schema', schemaTester(
-  [require.resolve('@khai96x/choose-text-editor/lib/editors.ts')],
-  schemas.CliArguments,
-  'CliArguments'
-))
