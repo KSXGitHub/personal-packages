4c848292fbcaa01de7eaf881bb572a7a495ec072
khai96_ <hvksmr1996@gmail.com>
2020-03-02 18:39:15 +0700

    Use export star syntax

--- packages/all/index.ts
+++ packages/all/index.ts
@@ -1,8 +1,3 @@
-import * as intervalObservableUniverse from '@khai96x/interval-observable-universe'
-export { intervalObservableUniverse }
-
-import * as myObservables from '@khai96x/my-observables'
-export { myObservables }
-
-import * as bumpPackageVersions from '@khai96x/bump-package-versions'
-export { bumpPackageVersions }
+export * as intervalObservableUniverse from '@khai96x/interval-observable-universe'
+export * as myObservables from '@khai96x/my-observables'
+export * as bumpPackageVersions from '@khai96x/bump-package-versions'
