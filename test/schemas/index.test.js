f305b84073401d3e7e0b5ddd81ba0d2f5b2b0e12
khai96_ <hvksmr1996@gmail.com>
2020-08-21 20:01:21 +0700

    Fix schemas test

--- test/schemas/index.test.js
+++ test/schemas/index.test.js
@@ -2,10 +2,10 @@
 const cmd = require('@tools/test-spawn')
 const bin = require.resolve('@ts-schema-autogen/cli/ts-schema-autogen')
 
-it('JavaScript Coding Style: Standard', () => {
+it('Schema Test', () => {
   cmd({
     defaultExecutable: 'node',
     argvPrefix: [bin, 'test'],
-    envMiddleName: 'SCHEMA_TEST'
+    envMiddleName: 'SCHEMA_TEST',
   })
 })
