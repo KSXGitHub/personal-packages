f233ed30e9108989c8eeb66e05429cbc20ac658f
khai96_ <hvksmr1996@gmail.com>
2020-08-22 11:38:15 +0700

    Create checkForUpdatesSingleCycle

--- packages/pacman/lib/check-for-updates.ts
+++ packages/pacman/lib/check-for-updates.ts
@@ -1,5 +1,8 @@
 import { spawn } from 'child_process'
+import { pipe, asyncFilter, asyncToArray } from 'iter-tools'
+import { readFile, writeFile, ensureFile, pathExists } from 'fs-extra'
 import { parseQuStream } from './parse-pacman-output'
+import { QuLine } from './types'
 
 /** Interface of sole parameter of {@link checkForUpdates} */
 export interface UpdateCheckerParams {
@@ -18,4 +21,55 @@ export function checkForUpdates(params: UpdateCheckerParams = {}) {
 
   const cp = spawn(packageManager, ['-Qu', ...additionalArguments])
   return parseQuStream(cp.stdout)
+}
+
+/** Interface of sole parameter of {@link checkForUpdatesSingleCycle} */
+export interface SingleCycleUpdateCheckerParams extends UpdateCheckerParams {
+  /** File to store due date */
+  readonly memoryFile: string
+  /** Cycle period (in milliseconds) */
+  readonly period: number
+}
+
+/** Interface of value yielded by {@link checkForUpdatesSingleCycle} */
+export interface SingleCycleUpdateCheckerReturn {
+  /** Out-of-date packages that are not ignored */
+  readonly updates: readonly QuLine[]
+  /** Next intended due date */
+  readonly nextDueDate: Date
+}
+
+/**
+ * If the date is due, check for updates, yield a single result, and update due date.
+ *
+ * This function is to run in an interval.
+ */
+export async function* checkForUpdatesSingleCycle(
+  params: SingleCycleUpdateCheckerParams,
+): AsyncGenerator<SingleCycleUpdateCheckerReturn> {
+  const { memoryFile, period } = params
+  const currentDate = Date.now()
+
+  if (!await pathExists(memoryFile)) {
+    await ensureFile(memoryFile)
+    await writeFile(memoryFile, String(currentDate))
+  }
+
+  const dueDate = Number(await readFile(memoryFile, 'utf8'))
+  if (currentDate >= dueDate) {
+    const updates = await pipe(
+      checkForUpdates(params),
+      asyncFilter(item => !item.ignored),
+      asyncToArray,
+    )
+
+    const nextDueDate = dueDate + period
+
+    yield {
+      updates,
+      nextDueDate: new Date(nextDueDate),
+    }
+
+    await writeFile(memoryFile, String(nextDueDate))
+  }
 }
