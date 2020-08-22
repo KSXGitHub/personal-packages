f77eb312f3cacb0aaf1aaeb0f34a1f9d60a2c44e
khai96_ <hvksmr1996@gmail.com>
2020-08-22 12:40:32 +0700

    Updates

--- shebang/assets/js/search.js
+++
@@ -1,3 +0,0 @@
-var typedoc = typedoc || {};
-            typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"65536":"Type literal"},"rows":[{"id":0,"kind":65536,"name":"__type","url":"globals.html#read_stream_options.__type","classes":"tsd-kind-type-literal tsd-parent-kind-variable","parent":"READ_STREAM_OPTIONS"}]};
