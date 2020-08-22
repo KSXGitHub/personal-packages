4463f9feed215a1da588055738abf16e7c65167b
khai96_ <hvksmr1996@gmail.com>
2020-08-22 10:13:11 +0700

    Use 'export * as'

--- packages/my-observables/index.ts
+++ packages/my-observables/index.ts
@@ -1,4 +1,2 @@
 export { operators } from '@khai96x/interval-observable-universe'
-
-import * as acAdapter from './lib/ac-adapter'
-export { acAdapter }
+export * as acAdapter from './lib/ac-adapter'
