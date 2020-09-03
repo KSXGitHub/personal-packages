d3fd0c288e22b79c686bf42d6e272c0d9584b175
khai96_ <hvksmr1996@gmail.com>
2020-09-03 10:48:34 +0700

    Run sane-fmt

--- test/git-utils/parse-porcelain-status.test.ts
+++ test/git-utils/parse-porcelain-status.test.ts
@@ -24,7 +24,7 @@ describe('parsePorcelainStatusValue', () => {
   it('success', () => {
     expect(
       ['??', ' M', ' D', 'A ', 'M ', 'D ', 'AD', 'AM', 'MD']
-        .map(parsePorcelainStatusValue)
+        .map(parsePorcelainStatusValue),
     ).toMatchSnapshot()
   })
 
