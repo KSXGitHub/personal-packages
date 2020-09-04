7aa200e55b0cf7b5f4f32b2e999f0728a639c562
khai96_ <hvksmr1996@gmail.com>
2020-09-04 12:20:39 +0700

    Take relative paths into account

--- packages/git-utils/lib/git-status.ts
+++ packages/git-utils/lib/git-status.ts
@@ -1,18 +1,26 @@
+import { relative } from 'path'
 import { spawn } from 'child_process'
+import { cwd } from 'process'
 import { pipe, asyncFilter, asyncMap } from 'iter-tools'
 import lines from './utils/lines'
-import { parsePorcelainStatus } from './parse-porcelain-status'
+import { Status, parsePorcelainStatus } from './parse-porcelain-status'
 
 export async function* gitStatus() {
   const cp = spawn('git', ['status', '--porcelain=v1'], {
     stdio: ['pipe', 'pipe', 'inherit'],
   })
 
+  const workingDirectory = cwd()
+
   yield* pipe(
     cp.stdout!,
     lines,
     asyncFilter(line => Boolean(line)),
     asyncMap(parsePorcelainStatus),
+    asyncMap(({ path, ...rest }): Status => ({
+      path: relative(workingDirectory, path),
+      ...rest,
+    })),
   )
 }
 
