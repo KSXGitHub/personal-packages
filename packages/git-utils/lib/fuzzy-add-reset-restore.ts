e7e8289fc647d7ca3e42c28cd93a649c50dc0ba8
khai96_ <hvksmr1996@gmail.com>
2020-09-04 08:47:33 +0700

    Create 'fuzzyAdd' and 'fuzzyReset'

---
+++ packages/git-utils/lib/fuzzy-add-reset-restore.ts
@@ -0,0 +1,149 @@
+import { spawn, spawnSync } from 'child_process'
+import { pipe, asyncFilter, asyncMap } from 'iter-tools'
+import shellEscape from 'shell-escape'
+import { trimNewLinesEnd } from './utils/trim-lf'
+import gitStatus from './git-status'
+import { Status, parsePorcelainStatus } from './parse-porcelain-status'
+
+export async function fuzzySelectChange(param: fuzzySelectChange.Param): Promise<fuzzySelectChange.Return> {
+  const {
+    fuzzyFinder,
+    when,
+  } = param
+
+  const inputLines = pipe(
+    gitStatus(),
+    asyncFilter(when),
+    asyncMap(status => status.plain),
+  )
+
+  const [fuzzyFinderProgram, ...fuzzyFinderArgs] = fuzzyFinder.split(/\s+/).filter(Boolean)
+  const cp = spawn(fuzzyFinderProgram, fuzzyFinderArgs, {
+    stdio: ['pipe', 'pipe', 'inherit'],
+  })
+
+  const { status, stdout } = await new Promise<{
+    readonly status: number
+    readonly stdout: string
+  }>(async (resolve, reject) => {
+    let stdout = ''
+    cp.on('error', reject)
+    cp.on('close', status => resolve({ status, stdout: trimNewLinesEnd(stdout) }))
+    cp.stdout.on('data', chunk => stdout += String(chunk))
+
+    for await (const line of inputLines) {
+      cp.stdin.write(line + '\n')
+    }
+  })
+
+  if (status) {
+    return {
+      tag: 'NonZeroReturn',
+      cpStatus: status,
+      message: `Command '${fuzzyFinder}' exited with code ${status}`,
+    }
+  }
+  if (!stdout) {
+    return {
+      tag: 'EmptyStdout',
+      message: `Command '${fuzzyFinder}' wrote nothing to stdout`,
+    }
+  }
+  return {
+    tag: 'Success',
+    gitStatus: stdout.split('\n').map(parsePorcelainStatus),
+  }
+}
+
+export namespace fuzzySelectChange {
+  export interface Param {
+    readonly fuzzyFinder: string
+    readonly when: (status: Status) => boolean
+  }
+
+  export type Return = Readonly<
+    | { tag: 'NonZeroReturn', cpStatus: number, message: string }
+    | { tag: 'EmptyStdout', message: string }
+    | { tag: 'Success', gitStatus: readonly Status[] }
+  >
+}
+
+function executeStringCommand(command: string, suffix: Iterable<string>): number {
+  const [program, ...prefix] = command.split(/\s+/).filter(Boolean)
+  if (!program) throw new Error('command is empty')
+  const { error, status } = spawnSync(command, [...prefix, ...suffix], { stdio: 'inherit' })
+  if (error) throw error
+  return status!
+}
+
+function logStringCommand(command: string, suffix: Iterable<string>): 0 {
+  console.info(command.trimEnd(), shellEscape([...suffix]))
+  return 0
+}
+
+const actionDict = { execute: executeStringCommand, log: logStringCommand } as const
+
+async function fuzzyAddReset(param: fuzzyAddReset.Param) {
+  const {
+    fuzzyFinder,
+    command,
+    action,
+    when,
+  } = param
+
+  const selectionResult = await fuzzySelectChange({
+    fuzzyFinder,
+    when,
+  })
+
+  switch (selectionResult.tag) {
+    case 'NonZeroReturn':
+      return selectionResult.cpStatus
+    case 'EmptyStdout':
+      console.error(selectionResult.message)
+      return 2
+    case 'Success':
+      break
+  }
+
+  const suffix = selectionResult.gitStatus.map(status => status.path)
+  return actionDict[action](command, suffix)
+}
+
+namespace fuzzyAddReset {
+  export interface Param {
+    readonly fuzzyFinder: string
+    readonly command: string
+    readonly action: 'log' | 'execute'
+    readonly when: (status: Status) => boolean
+  }
+
+  export type CustomizedParam<CommandField extends string> =
+    Pick<Param, 'fuzzyFinder' | 'action'> & Record<CommandField, string>
+}
+
+export function fuzzyAdd(param: fuzzyAdd.Param) {
+  const { addCommand, ...rest } = param
+  return fuzzyAddReset({
+    command: addCommand,
+    when: ({ value }) => value.plain === '??' || value.unstaged !== ' ',
+    ...rest,
+  })
+}
+
+export namespace fuzzyAdd {
+  export type Param = fuzzyAddReset.CustomizedParam<'addCommand'>
+}
+
+export function fuzzyReset(param: fuzzyReset.Param) {
+  const { resetCommand, ...rest } = param
+  return fuzzyAddReset({
+    command: resetCommand,
+    when: ({ value }) => value.plain !== '??' && value.staged !== ' ',
+    ...rest,
+  })
+}
+
+export namespace fuzzyReset {
+  export type Param = fuzzyAddReset.CustomizedParam<'resetCommand'>
+}
